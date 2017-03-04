84ec035635842fda589f828b3b1345b38ff8b6ab
Daniel Medina <daniel.medina11@protonmail.com>
2017-03-04 09:55:59 +0000

    working on SASS integration

---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Test</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'index',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+/** Dependencies */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+/** Components */
+import Index from '@/components/Index'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: Index
+        }
+    ]
+})
